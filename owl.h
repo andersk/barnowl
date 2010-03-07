dd16f4906a05ce383b2287253ba44000ded682f3
Anders Kaseorg <andersk@mit.edu>
2010-03-07 04:14:06 -0500

    Use bool from <stdbool.h> instead of an ad-hoc definition in libfaim/aim.h.
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- owl.h
+++ owl.h
@@ -9,6 +9,7 @@
 #ifndef INC_OWL_H
 #define INC_OWL_H
 
+#include <stdbool.h>
 #ifndef OWL_PERL
 #include <curses.h>
 #endif
@@ -35,7 +36,6 @@
 typedef void WINDOW;
 /* logout is defined in FreeBSD. */
 #define logout logout_
-/* aim.h defines bool */
 #define HAS_BOOL
 #include <perl.h>
 #include "owl_perl.h"
